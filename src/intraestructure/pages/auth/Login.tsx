cc74b5d03c468b33fc1f1917bfee2ec1cbd26a55
luciano-dlr <delarubialuciano@gmail.com>
2024-04-03 16:23:12 -0300

    making mock

--- src/intraestructure/pages/auth/Login.tsx
+++ src/intraestructure/pages/auth/Login.tsx
@@ -1,9 +1,20 @@
-import React from 'react'
 
 export const Login = () => {
   return (
-    <div>
-      <text>Soy el login</text>
+    <div className="h-screen flex justify-center items-center gap-4">
+
+      <div className="bg-black">
+
+        <h2 className="text-slate-200	text-xl">Login</h2>
+
+      </div>
+
+      <div className="bg-black">
+
+        <p className="text-slate-200	text-xl">Imagen</p>
+
+      </div>
+
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.tsx'
+// import { App } from './App.tsx'
 import './index.css'
 import { Navigator } from './intraestructure/navigation/Navigator.tsx'
 import { BrowserRouter } from 'react-router-dom'
